1555da54204aebba459682f35511ec8d92c3a4e4
Luís Gustavo <luisgsilva270@gmail.com>
2023-11-22 20:35:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import scale
 %matplotlib inline
+
+# 20:34
+
+features_list = ('HG','AG','PH','PD','PA','MaxH','MaxD','MaxA','AvgH','AvgD','AvgA')
+
+k_best_features = SelectKBest(k='all')
+k_best_features.fit_transform(features, labels)
+k_best_features_scores = k_best_features.scores_
+raw_pairs = zip(features_list[1:], k_best_features_scores)
+ordered_pairs = list(reversed(sorted(raw_pairs, key=lambda x: x[1])))
+
+k_best_features_final = dict(ordered_pairs[:15])
+best_features = k_best_features_final.keys()
+print ('')
+print ("Melhores features:")
+print (k_best_features_final)
